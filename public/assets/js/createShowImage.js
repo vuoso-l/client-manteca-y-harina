fd18014c8d30416505a37922f58cb444f26242e0
vuoso-l <vuosolucas@gmail.com>
2021-12-09 17:43:30 -0300

    Add h2

--- public/assets/js/createShowImage.js
+++ public/assets/js/createShowImage.js
@@ -1,11 +1,16 @@
-const containerGallery = document.querySelector(".gallery-container");
-const gallery = document.querySelector(".gallery");
 const container = document.querySelector(".container");
+const containerGallery = document.querySelector(".gallery-container");
+const h2 = document.querySelector(".gallery-container h2");
+const gallery = document.querySelector(".gallery");
 const dotContainer = document.querySelector(".dotContainer");
 
 const createShowImage = (images) => {
     container.style.display = "none";
     containerGallery.style.display = "flex";
+
+    h2.innerHTML = `
+    <h2>${images.thematic}</h2>
+    `
 
     gallery.innerHTML += `    
     <div class="mySlides fades">

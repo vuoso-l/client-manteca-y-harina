783946545cd94da4990a9b9986978901bd4a6398
vuoso-l <vuosolucas@gmail.com>
2021-12-17 19:46:29 -0300

    Add imgError

--- public/assets/js/upload.js
+++ public/assets/js/upload.js
@@ -1,13 +1,26 @@
+const containerUpload = document.querySelector(".containerUpload");
 const form = document.querySelector("form");
-import {closeSession} from "./closeSession.js";
+import { closeSession } from "./closeSession.js";
 let tokenJWT = localStorage.getItem("jwt");
-console.log(tokenJWT);
+
 //una vez funcione el bloqueo y acceso con el token
 //ver de armar el form con js para bloquear el acceso
 //si no se tiene el token
+
+onload = () => {
+    if (!tokenJWT) {
+        containerUpload.innerHTML = `
+            <section>
+                <p>No tienes acceso a esta sección!</p>
+                <img class="imgError" src="/public/assets/imgMyH/logo.jpg" alt="Logo m&h"></img>
+            </section>   
+            `;
+    }
+}
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    uploadImage();    
+    uploadImage();
 })
 
 closeSession();

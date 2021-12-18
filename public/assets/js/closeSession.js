d53ae24f85383a8b46cf30cc93f9bbd502a0ce19
vuoso-l <vuosolucas@gmail.com>
2021-12-18 16:40:55 -0300

    Add message from sweetAlert

--- public/assets/js/closeSession.js
+++ public/assets/js/closeSession.js
@@ -2,8 +2,20 @@ const closeSes = document.querySelector("#closeSession");
 
 const closeSession = () => {
     closeSes.addEventListener("click", () => {
-        localStorage.removeItem("token");
-        location.href = "../../../index.html";
+        Swal.fire({
+            title: 'Estás segura que querés cerrar sesión?',
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, cerrar!',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                localStorage.removeItem("token");
+                location.href = "../../../index.html";
+            }
+        })
     })
 }
 

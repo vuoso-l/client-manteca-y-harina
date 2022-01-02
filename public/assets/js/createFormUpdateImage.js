affe9089851f7add130d742851b45b201de6f718
vuoso-l <vuosolucas@gmail.com>
2022-01-02 20:06:21 -0300

    Change values from select button

--- public/assets/js/createFormUpdateImage.js
+++ public/assets/js/createFormUpdateImage.js
@@ -23,16 +23,16 @@ const createFormUpdateImage = (image) => {
                     <select name="section" id="section">
                         <option value="" selected disabled>${image.section}</option>
                         <option value="home">Sección para home</option>
-                        <option value="tortasVarias">Sección para tortas varias</option>
+                        <option value="tortas-Varias">Sección para tortas varias</option>
                         <option value="navidad">Sección para navidad</option>
-                        <option value="tortasBodas">Sección para tortas de bodas</option>
-                        <option value="tortasMusicales">Sección para tortas musicales</option>
+                        <option value="tortas-Bodas">Sección para tortas de bodas</option>
+                        <option value="tortas-Musicales">Sección para tortas musicales</option>
                         <option value="comuniones">Sección para comuniones</option>
                         <option value="infantiles">Sección para infantiles</option>
-                        <option value="tartasDulces">Sección para tartas dulces</option>
+                        <option value="tartas-Dulces">Sección para tartas dulces</option>
                         <option value="postres">Sección para postres</option>
                         <option value="alfajores">Sección para alfajores</option>
-                        <option value="porcelanaFria">Sección para porcelana fría</option>
+                        <option value="porcelana-Fria">Sección para porcelana fría</option>
                     </select>
                     <ul id="sectionUpdateError"></ul>
                 </div>
@@ -40,16 +40,16 @@ const createFormUpdateImage = (image) => {
                     <label for="thematic">Temática</label>
                     <select name="thematic" id="thematic">
                         <option value="" selected disabled>${image.thematic}</option>
-                        <option value="tortasVarias">Sección para tortas varias</option>
+                        <option value="tortas-Varias">Sección para tortas varias</option>
                         <option value="navidad">Sección para navidad</option>
-                        <option value="tortasBodas">Sección para tortas de bodas</option>
-                        <option value="tortasMusicales">Sección para tortas musicales</option>
+                        <option value="tortas-Bodas">Sección para tortas de bodas</option>
+                        <option value="tortas-Musicales">Sección para tortas musicales</option>
                         <option value="comuniones">Sección para comuniones</option>
                         <option value="infantiles">Sección para infantiles</option>
-                        <option value="tartasDulces">Sección para tartas dulces</option>
+                        <option value="tartas-Dulces">Sección para tartas dulces</option>
                         <option value="postres">Sección para postres</option>
                         <option value="alfajores">Sección para alfajores</option>
-                        <option value="porcelanaFria">Sección para porcelana fría</option>
+                        <option value="porcelana-Fria">Sección para porcelana fría</option>
                     </select>
                     <ul id="thematicUpdateError"></ul>
                 </div>

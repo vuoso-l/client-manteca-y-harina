c4f375e8fd12813b48f9bd5196bef7843a0e7801
vuoso-l <vuosolucas@gmail.com>
2022-02-01 12:54:37 -0300

    Added modal close functions and reorganized the info

--- public/assets/js/createShowInfoOrders.js
+++ public/assets/js/createShowInfoOrders.js
@@ -8,54 +8,53 @@ const createShowInfoOrders = () => {
   containerGallery.style.display = "flex";
 
   btnOpen.addEventListener("click", () => {
-      galleryInfo.style.display = "flex";
-  })
+    galleryInfo.style.display = "flex";
+  });
 
   galleryInfo.innerHTML = `
     
         <div class="modal-container">
-            <h3>Importante</h3>
+            <h2>Importante</h2>
             <button class="modal-close">X</button>
-            <ul>
-                <li>Cada torta incluye 3 capas de relleno con altura aprox. de 11 a 12 cm.</li>
+            <h3>Tortas</h3>
+            <ul class="cake">
+                <li>Cada una incluye 3 capas de relleno</li>
                 <li>Bizcochuelo de vainilla o chocolate</li>
-                <li>El diseño y la temática es a elección</li>
+                <li>12 cm. a 24cm. de relleno</li>
+                <li>Diseño y temática a elección</li>
                 <li>Incluye velita</li>
                 <li>El presupuesto no incluye packaging individual</li>
                 <li>Decoraciones con adornos tiene costo adicional</li>
-            </ul>
-            <h3>Tortas</h3>
-            <ul>
-                <li>12 cm. de relleno</li>
-                <li>15 cm. de relleno</li>
-                <li>18 cm. de relleno</li>
-                <li>20 cm. de relleno</li>
-                <li>22 cm. de relleno</li>
-                <li>24 cm. de relleno</li>
-            </ul>
+            </ul>            
             <h3>Letter o number cake</h3>
-            <p>3 capas de sablée de vainilla, 2 rellenos y cobertura con deco final a elección</p>
-            <ul>
+            <p>3 capas de sablée de vainilla, 2 rellenos y cobertura con deco final a elección. Medidas:</p>
+            <ul class="cake">
                 <li>15 x 20 cm.</li>
                 <li>20 x 25 cm.</li>
             </ul>
             <h3>Alfajores</h3>
-            <ul>
-                <li>Maicena</li>
-                <li>Chocolate</li>
-                <li>Nuez</li>
-                <li>Dulce de leche y chocolate</li>
+            <ul class="cookies">
+                <li>Maicena - Chocolate - Nuez - Dulce de leche y chocolate</li>
             </ul>
             <h3>¿Qué más podés agregarle a tus festejos?</h3>
-            <p>Cookies - cakepops - Oreos bañadas - cupcakes - icepops</p>
-            <p>Torta brownie - Lemon pie - Cheese cake - Tarta de frutas frescas</p>
+            <ul class="cookies">
+                <li>Cookies - cakepops - Oreos bañadas - cupcakes - icepops</li>
+                <li>Torta brownie - Lemon pie - Cheese cake - Tarta de frutas frescas</li>
+            </ul>
         </div>
     `;
 
-    const btnClose = document.querySelector(".modal-close");
-    btnClose.addEventListener("click", () => {
-        galleryInfo.style.display = "none";
-    })
+  document.querySelector(".modal-container").addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
+
+  document.querySelector(".modal-close").addEventListener("click", () => {
+    galleryInfo.style.display = "none";
+  });
+  
+  galleryInfo.addEventListener("click", () => {
+    galleryInfo.style.display = "none";
+  });
 };
 
 export { createShowInfoOrders };
